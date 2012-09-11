c465159becd63d0bafb5c9547614b310d482ff3a
ingo <ingo.walz@googlemail.com>
2012-09-11 19:26:22 +0200

    Whatever...

---
+++ tests/Bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+if (!(@include_once __DIR__ . '/../vendor/autoload.php') && 
+		!(@include_once __DIR__ . '/../../../autoload.php')
+)
+{
+	throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
+
+\ZendServerAPI\Startup::setConfigPath(__DIR__.'/_files/config/servers.php');
+    
